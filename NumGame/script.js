5176bf2fc4e4f35cef9c02624ec753d6008b1a4b
Muhammad-Arshadh <arshadh1369@gmail.com>
2024-11-21 21:26:43 +0530

    modified script.js

--- NumGame/script.js
+++ NumGame/script.js
@@ -1,3 +1,7 @@
+var msg1 = document.getElementById("message1")
+var msg2 = document.getElementById("message2")
+var msg3 = document.getElementById("message3")
+
 var answer = math.floor(math.random()*100) + 1;
 var no_of_guesses = 0;
 var guessed_num = [];
