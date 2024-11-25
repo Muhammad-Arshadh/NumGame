a42a860d730c0e6953797d468af5e41f4575dc1d
Muhammad-Arshadh <arshadh1369@gmail.com>
2024-11-25 23:31:46 +0530

    modified script.js

--- NumGame/script.js
+++ NumGame/script.js
@@ -1,4 +1,4 @@
-var msg1 = document.getElementById("message1")
+var lowAndHigh = document.getElementById("message1")
 var msg2 = document.getElementById("message2")
 var msg3 = document.getElementById("message3")
 var button = document.getElementById("btn")
@@ -16,18 +16,18 @@ function play(){
         no_of_guesses += 1;
 
         if(user_guess < answer){
-            msg1.textContent = "Your guess is too low";
+            lowAndHigh.textContent = "Your guess is too low";
             msg2.textContent = "No of guess : " + no_of_guesses;
             msg3.textContent = "Guessed No : " + guessed_num;
 
         }
         else if(user_guess > answer){
-            msg1.textContent = "Your guess is too high";
+            lowAndHigh.textContent = "Your guess is too high";
             msg2.textContent = "No of guess : " + no_of_guesses;
             msg3.textContent = "Guessed No : " + guessed_num;
         }
         else if(user_guess==answer){
-            msg1.textContent = "YOU GOT IT!";
+            lowAndHigh.textContent = "YOU GOT IT!";
             msg2.textContent = "No of guess : " + no_of_guesses;
             msg3.textContent = "Guessed No : " + guessed_num;
             button.disabled  = true;
