59e32b14ef19ddb30edc7d008767a4e5b89ffde9
Muhammad-Arshadh <arshadh1369@gmail.com>
2024-12-01 14:05:52 +0530

    Style: Improves code formatting in script.js
    Removes unnecessary spaces and standardizes code style for improved readability and consistency.  Focuses on spacing around operators and conditional statements.

--- script.js
+++ script.js
@@ -3,36 +3,36 @@ var NoOfGuess = document.getElementById("message2")
 var GuessedNo = document.getElementById("message3")
 var button = document.getElementById("btn")
 var input = document.getElementById("guess")
-var answer = Math.floor(Math.random()*100) + 1;
+var answer = Math.floor(Math.random() * 100) + 1;
 var no_of_guesses = 0;
 var guessed_num = [];
 
-function play(){ 
+function play() {
     var user_guess = input.value;
-    if(user_guess<1||user_guess>100||input.value === ""){ //Need to disable button and hover before entering a valid input 
+    if (user_guess < 1 || user_guess > 100 || input.value === "") { //Need to disable button and hover before entering a valid input 
         alert("Enter a number between 1 to 100")
         button.style.pointerEvents = "none";
     }
-    else{
+    else {
         guessed_num.push(user_guess);
         no_of_guesses += 1;//Need to add limit
 
-        if(user_guess < answer){
+        if (user_guess < answer) {
             yourGuess.textContent = "Your guess is too low";
             NoOfGuess.textContent = "No of guess : " + no_of_guesses;
             GuessedNo.textContent = "Guessed No : " + guessed_num;
 
         }
-        else if(user_guess > answer){
+        else if (user_guess > answer) {
             yourGuess.textContent = "Your guess is too high";
             NoOfGuess.textContent = "No of guess : " + no_of_guesses;
             GuessedNo.textContent = "Guessed No : " + guessed_num;
         }
-        else if(user_guess==answer){
+        else if (user_guess == answer) {
             yourGuess.textContent = "YOU GOT IT!";
             NoOfGuess.textContent = "No of guess : " + no_of_guesses;
             GuessedNo.textContent = "Guessed No : " + guessed_num;
-            button.disabled  = true;
+            button.disabled = true;
             button.style.pointerEvents = "none";
         }
     }
